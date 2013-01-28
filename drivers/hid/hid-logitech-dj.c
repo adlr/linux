9cf8fd794580067e94557bdcbd6bf73299df0922
Andrew de los Reyes <adlr@chromium.org>
2013-01-27 16:50:57 -0800

    remove dead code (review comment)
    Change-Id: I5c6a025f51dadecedb8a4a9529fef3e2bafb14e6

--- drivers/hid/hid-logitech-dj.c
+++ drivers/hid/hid-logitech-dj.c
@@ -389,12 +389,6 @@ static void logi_dj_recv_add_djhid_device(struct dj_receiver_dev *djrcv_dev,
 	djrcv_dev->paired_dj_devices[dj_report->device_index] = dj_dev;
 
 	return;
-
-hid_add_device_fail:
-	djrcv_dev->paired_dj_devices[dj_report->device_index] = NULL;
-	kfree(dj_dev);
-dj_device_allocate_fail:
-	hid_destroy_device(dj_hiddev);
 }
 
 static void delayedwork_callback(struct work_struct *work)
